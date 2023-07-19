5483d954224a3d4d33178be0ea8feeccd2c3d9d8
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-07-18 22:19:15 -0300

    feat: added main

---
+++ src/ArrayList/Main.java
@@ -0,0 +1,11 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> contacts = new ArrayList<>(20);
+    }
+}
