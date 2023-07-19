b986ca31c7409d575ca022ce40acda92bbacc7a6
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-07-19 20:11:54 -0300

    feat(collections): added collections material

---
+++ src/Collections/Domain/Manga.java
@@ -0,0 +1,39 @@
+package Collections.Domain;
+
+import java.util.Objects;
+
+public class Manga {
+    private Long id;
+    private String name;
+    private double price;
+
+    public Manga(Long id, String name, double price) {
+        Objects.requireNonNull(id);
+        Objects.requireNonNull(name);
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Manga manga = (Manga) o;
+        return Double.compare(manga.price, price) == 0 && Objects.equals(id, manga.id) && Objects.equals(name, manga.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price);
+    }
+
+    @Override
+    public String toString() {
+        return "Manga{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
