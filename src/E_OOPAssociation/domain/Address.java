b6f003af2ad7e4588dbe1e330184580d3f9ef4b3
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-07-24 17:35:59 -0300

    feat: added oop exercises

---
+++ src/E_OOPAssociation/domain/Address.java
@@ -0,0 +1,17 @@
+package E_OOPAssociation.domain;
+
+public class Address {
+    private String address;
+
+    public Address(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
