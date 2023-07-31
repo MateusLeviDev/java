787eee8c93a007398f8461c8a9080ad54b4e537e
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-07-31 20:59:15 -0300

    feat: generics

---
+++ src/J_Generics/test/WildCardTest01.java
@@ -0,0 +1,36 @@
+package J_Generics.test;
+
+import java.util.List;
+
+abstract class Animal {
+    public abstract void consulta();
+}
+
+class Dog extends Animal {
+
+    @Override
+    public void consulta() {
+        System.out.println("Consultando doguito.");
+    }
+}
+
+class Cat extends Animal {
+
+    @Override
+    public void consulta() {
+        System.out.println("Consultando gatito");
+    }
+}
+
+public class WildCardTest01 {
+    public static void main(String[] args) {
+        List<Dog> dogs = List.of(new Dog(), new Dog());
+        List<Cat> cats = List.of(new Cat(), new Cat());
+    }
+
+    private static void printConsulta (List<? extends Animal> animals){
+        for (Animal animal : animals) {
+            animal.consulta();
+        }
+    }
+}
