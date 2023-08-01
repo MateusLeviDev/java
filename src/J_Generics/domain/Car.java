cbab64c52a5c4e9b74df6d6acc1e9bbe9215ec27
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-08-01 17:10:42 -0300

    feat: add generics. working w classes

---
+++ src/J_Generics/domain/Car.java
@@ -0,0 +1,16 @@
+package J_Generics.domain;
+
+public class Car {
+    private String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
