de9ae633d8d0d0c455b5f83e652fd1a3686995d8
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-08-06 02:19:41 -0300

    refactor: added ney syntax

--- src/K_Streams/domain/LightNovel.java
+++ src/K_Streams/domain/LightNovel.java
@@ -1,5 +1,7 @@
 package K_Streams.domain;
 
+import java.util.Objects;
+
 public class LightNovel {
     private String name;
     private double price;
@@ -7,6 +9,19 @@ public class LightNovel {
     public LightNovel(String name, double price) {
         this.name = name;
         this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LightNovel that = (LightNovel) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 
     public String getName() {
