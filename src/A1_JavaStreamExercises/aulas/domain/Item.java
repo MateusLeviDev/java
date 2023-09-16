862e29b454dbc85ce4cac992bc55a99ca49abfec
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-09-16 20:55:21 -0300

    feat: added stream exercises #2

---
+++ src/A1_JavaStreamExercises/aulas/domain/Item.java
@@ -0,0 +1,36 @@
+package A1_JavaStreamExercises.aulas.domain;
+
+public class Item {
+    private Integer id;
+    private String name;
+
+    public Item(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
+
