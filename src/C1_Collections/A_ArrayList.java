6e883b75ecfdba334c5cdf6cf6d03b9bec88a906
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-10-07 16:34:03 -0300

    feat[Collections]: Iterating over Collections #2

---
+++ src/C1_Collections/A_ArrayList.java
@@ -0,0 +1,25 @@
+package C1_Collections;
+
+import java.util.*;
+
+public class A_ArrayList {
+    public static void main(String[] args) {
+        ArrayList<Object> objects = new ArrayList<>();
+
+        //adding objects
+        objects.add("Apple");
+        objects.add("Banana");
+
+        //iterating over list
+        objects.forEach(System.out::println);
+        objects.parallelStream().forEach(System.out::println);
+
+        //iterating over map
+        Map<Integer, String> names = new HashMap<>();
+        names.put(1,"Levi");
+        names.put(2,"Souza");
+        names.put(3,"Omar");
+
+        names.forEach((id, name) -> System.out.println("Id: " + id + "\nname: " + name));
+    }
+}
