9bff7a31405ecd3fe1401945e0be2abe6dbd9ee3
levi <101754313+MateusLeviDev@users.noreply.github.com>
2023-10-16 14:13:23 -0300

    `chore`: added new collections exercises

---
+++ src/C1_Collections/RemovingItens.java
@@ -0,0 +1,22 @@
+package C1_Collections;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class RemovingItens {
+
+	public static void main(String[] args) {
+//      removing itens from a list within loop
+		
+		List<String> fruits = new ArrayList<String>(List.of("Banana", "Apple", "Strawberry"));
+		
+		Iterator<String> fuitIterator = fruits.iterator();
+		while (fuitIterator.hasNext()) {
+			String string = (String) fuitIterator.next();
+			
+		}
+
+	}
+
+}
