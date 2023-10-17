e8dbfbf14fdb16d45e6373bd80f5537403017477
levi <101754313+MateusLeviDev@users.noreply.github.com>
2023-10-17 15:01:46 -0300

    `chore`: added new collections exercises - List, Map, Set

--- src/C1_Collections/ListVsSet.java
+++ src/C1_Collections/ListVsSet.java
@@ -9,8 +9,10 @@ public class ListVsSet {
 	public static void main(String[] args) {
 		List<String> numbers = List.of("1", "2", "3", "4", "1");
 		numbers.forEach(System.out::println);
-		System.out.println("*******************************************");
-		Set<String> numbers2 = new HashSet<String>(Arrays.asList("1","2","3","4","1","2","5"));
+		
+		System.out.println("***************************************");
+		
+		Set<String> numbers2 = new HashSet<String>(Arrays.asList("1", "2", "3", "4", "1", "2", "5"));
 		numbers2.forEach(System.out::println);
 	}
 }
