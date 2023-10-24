636943f17983e20369582fd257e79e82105d331b
levi <101754313+MateusLeviDev@users.noreply.github.com>
2023-10-24 13:53:49 -0300

    `feat`: added Java @Stream and @Lambda exercises from Deitel

---
+++ src/Deitel_Java/POO_Polimorfismo/SalariedEmployee.java
@@ -0,0 +1,36 @@
+package Deitel_Java.POO_Polimorfismo;
+
+public class SalariedEmployee extends Employee {
+
+	private double weeklySalary;
+
+	public SalariedEmployee(String firstName, String lastName, String socialsecurityNumber, double weeklySalary) {
+		super(firstName, lastName, socialsecurityNumber);
+
+		if (weeklySalary < 0.0)
+			throw new IllegalArgumentException("Weekly salary must be >= 0.0");
+
+		this.weeklySalary = weeklySalary;
+	}
+
+	public double getWeeklySalary() {
+		return weeklySalary;
+	}
+
+	public void setWeeklySalary(double weeklySalary) {
+		if (weeklySalary < 0.0)
+			throw new IllegalArgumentException("Weekly salary must be >= 0.0");
+		this.weeklySalary = weeklySalary;
+	}
+
+	@Override
+	public double earnings() {
+		return getWeeklySalary();
+	}
+
+	@Override
+	public String toString() {
+		return "SalariedEmployee [weeklySalary=" + weeklySalary + "]";
+	}
+
+}
