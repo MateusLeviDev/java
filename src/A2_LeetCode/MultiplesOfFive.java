461934b4ef7c2722e867674218e400bdc2d4055b
levi <101754313+MateusLeviDev@users.noreply.github.com>
2023-11-10 14:44:56 -0300

    `feat`: added quickly logic exercises

---
+++ src/A2_LeetCode/MultiplesOfFive.java
@@ -0,0 +1,11 @@
+package A2_LeetCode;
+
+public class MultiplesOfFive {
+	public static void main(String[] args) {
+		for (int i = 0; i <= 100; i++) {
+			if (!(i % 5 == 0)) {
+				System.out.println(i);
+			}
+		}
+	}
+}
