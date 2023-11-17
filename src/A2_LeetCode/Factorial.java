94457124af73ea5b4c6ea43b51e1cc8a20e7990e
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-11-17 20:52:14 -0300

    `feat`: added codes examples #23

---
+++ src/A2_LeetCode/Factorial.java
@@ -0,0 +1,23 @@
+package A2_LeetCode;
+
+import java.util.stream.LongStream;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(factorialUsingStreams(5));
+    }
+
+    public static long factorialUsingStreams(int n) {
+        return LongStream.rangeClosed(1, n)
+                .reduce(1, (long x, long y) -> x * y);
+    }
+
+    public static int fatorial(int n) {
+        if (n <= 1) {
+            return 1;
+        } else {
+            return n * fatorial(n - 1);
+        }
+    }
+}
