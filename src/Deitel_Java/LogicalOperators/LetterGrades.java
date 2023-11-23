d9eb409295f4e3c5edeb3581f8006c1df6e623ca
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-11-23 18:46:53 -0300

    `feat`: added codes examples #25

--- src/Deitel_Java/LogicalOperators/LetterGrades.java
+++ src/Deitel_Java/LogicalOperators/LetterGrades.java
@@ -3,44 +3,53 @@ package Deitel_Java.LogicalOperators;
 import java.util.Scanner;
 
 public class LetterGrades {
-	public static void main(String[] args) {
-		int total = 0;
-		int gradeCounter = 0;
-		int aCount = 0;
-		int bCount = 0;
-		int cCount = 0;
-		int dCount = 0;
-		int fCount = 0;
+    public static void main(String[] args) {
+        int total = 0;
+        int gradeCounter = 0;
+        int aCount = 0;
+        int bCount = 0;
+        int cCount = 0;
+        int dCount = 0;
+        int fCount = 0;
 
-		Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-		System.out.printf("%s%n%s%n %s%n %s%n", "Enter the integer grades in the range 0-100.",
-				"Type the end-of-file indicator to terminate input:",
-				"On UNIX/Linux/Mac OS X type <Ctrl> d then press Enter", "On Windows type <Ctrl> z then press Enter");
+        System.out.printf("%s%n%s%n %s%n %s%n", "Enter the integer grades in the range 0-100.",
+                "Type the end-of-file indicator to terminate input:",
+                "On UNIX/Linux/Mac OS X type <Ctrl> d then press Enter", "On Windows type <Ctrl> z then press Enter");
 
-		while (scanner.hasNext()) {
-			int grade = scanner.nextInt();
-			total += grade;
-			++gradeCounter;
+        while (scanner.hasNext()) {
+            int grade = scanner.nextInt();
+            total += grade;
+            ++gradeCounter;
 
-			switch (grade / 10) {
-			case 9:
-			case 10:
-				++aCount;
-				break;
-			case 8:
-				++bCount;
-				break;
-			case 7:
-				++cCount;
-			case 6:
-				++dCount;
-				break;
-			default:
-				++fCount;
-				break;
-			}
-		}
+            switch (grade / 10) {
+                case 9:
+                case 10:
+                    ++aCount;
+                    break;
+                case 8:
+                    ++bCount;
+                    break;
+                case 7:
+                    ++cCount;
+                case 6:
+                    ++dCount;
+                    break;
+                default:
+                    ++fCount;
+                    break;
+            }
+        }
+        System.out.printf("%nGrade Report:%n");
 
-	}
+        if (gradeCounter != 0) {
+            double average = (double) total / gradeCounter;
+
+            System.out.printf("Total of the %d grades entered is %d%n", gradeCounter, total);
+            System.out.printf("Class average is %.2f%n", average);
+            System.out.printf("%n%s%n%s%d%n%s%d%n%s%d%n%s%d%n%s%d%n", "Number of students who received each grade:",
+                    "A: ", aCount, "B: ", bCount, "C: ", cCount, "D: ", dCount, "F: ", fCount);
+        } else System.out.println("No grades were entered");
+    }
 }
