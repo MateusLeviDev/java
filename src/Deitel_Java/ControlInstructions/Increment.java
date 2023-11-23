2c1abb62b1d23ac5aea556fbbdc249db8ae437e2
levi <101754313+MateusLeviDev@users.noreply.github.com>
2023-11-23 14:57:49 -0300

    `feat:` added codes examples #24

---
+++ src/Deitel_Java/ControlInstructions/Increment.java
@@ -0,0 +1,19 @@
+package Deitel_Java.ControlInstructions;
+
+public class Increment {
+	public static void main(String[] arg) {
+		int c = 5;
+
+		// operador pós icremento
+
+		System.out.printf("c before postincrement: %d%n", c); // print 5
+		System.out.printf("         postincrement c: %d%n", c++); // print 5
+		System.out.printf("c after postincrement: %d%n", c); // print 6
+
+		System.out.println();
+
+		System.out.printf(" c before preincrement: %d%n", c); // print 5
+		System.out.printf(" preincrementing c: %d%n", ++c); // print 6
+		System.out.printf(" c after preincrement: %d%n", c); // print 6
+	}
+}
---
+++ src/Deitel_Java/ControlInstructions/NestedControlStatements.java
@@ -0,0 +1,36 @@
+package Deitel_Java.ControlInstructions;
+
+import java.util.Scanner;
+
+public class NestedControlStatements {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int passes = 0;
+		int failures = 0;
+		int studentsCounter = 1;
+
+		while (studentsCounter <= 10) {
+			System.out.print("insert notes: ");
+			int grade = scanner.nextInt();
+
+			if (grade < 0 || grade > 10) {
+				System.out.println("Insira um valor entre 1 e 10.");
+				continue; // Volta para o início do loop sem incrementar studentsCounter
+			}
+			if (grade >= 7) {
+				passes++;
+			} else {
+				failures++;
+			}
+
+			studentsCounter++;
+		}
+
+		System.out.println("Alunos aprovados: " + passes);
+		System.out.println("Alunos reprovado: " + failures);
+
+		if (passes > 8)
+			System.out.println("Bonus!");
+	}
+}
