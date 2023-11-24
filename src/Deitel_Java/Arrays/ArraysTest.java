9ac3a113049b83061778d03b3337a29a577063f6
levi <101754313+MateusLeviDev@users.noreply.github.com>
2023-11-24 11:55:03 -0300

    `feat`: added Deitel exercises #26

---
+++ src/Deitel_Java/Arrays/ArraysTest.java
@@ -0,0 +1,34 @@
+package Deitel_Java.Arrays;
+
+public class ArraysTest {
+	public static void main(String[] args) {
+		int[] arr = new int[10];
+		final int ARRAY_LENGTH = 10;
+
+		System.out.printf("%s%8s%n", "Index", "Value");
+
+		for (int i = 0; i < arr.length; i++)
+			System.out.printf("%5d%8d%n", i, arr[i]);
+
+		System.out.println("=========================");
+		int[] arr2 = { 1, 5, 7, 9, 3, 56, 24, 78, 71 };
+
+		System.out.printf("%s%8s%n", "Index", "Value");
+
+		for (int i = 0; i < arr2.length; i++)
+			System.out.printf("%5d%8d%n", i, arr2[i]);
+
+		System.out.println("=========================");
+
+		int[] arr3 = new int[ARRAY_LENGTH];
+
+		for (int i = 0; i < arr3.length; i++)
+			arr3[i] = 2 + 2 * i;
+
+		System.out.printf("%s%8s%n", "Index", "Value");
+
+		for (int i = 0; i < arr3.length; i++)
+			System.out.printf("%5d%8d%n", i, arr3[i]);
+
+	}
+}
--- src/Deitel_Java/DiveMethods/MethodsStatic.java
+++ src/Deitel_Java/DiveMethods/MethodsStatic.java
@@ -2,13 +2,16 @@ package Deitel_Java.DiveMethods;
 
 public class MethodsStatic {
 	public static void main(String[] args) {
-		//Math class methods
+		// Math class methods
 		System.out.printf("%.2f", Math.sqrt(500));
-		int x = (int) Math.max(1, 2);
-		System.out.println(x);
 		
+		System.out.println();
+
+		System.out.println(Math.max(6, 2));
+
 		System.out.println(Math.pow(2, 2));
-		
-		System.out.println(Math.log(100));
+
+		System.out.printf("%.2f", Math.log(100));
+
 	}
 }
