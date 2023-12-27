eec90d30e247fa8577a12ee1119fba69f3eb2d96
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-12-27 13:01:07 -0300

    Initial commit

---
+++ PicPay/src/main/java/java/demo/com/PicPay/PicPayApplication.java
@@ -0,0 +1,13 @@
+package java.demo.com.PicPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicPayApplication.class, args);
+	}
+
+}
---
+++ PicPay/src/test/java/java/demo/com/PicPay/PicPayApplicationTests.java
@@ -0,0 +1,13 @@
+package java.demo.com.PicPay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
