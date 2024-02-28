f876fbcd2aa472bcbfe13be86aa8e1dacc85d80b
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2024-02-28 18:28:56 -0300

    initial commit

---
+++ api-cdc/src/main/java/org/levi/apicdc/apicdc/ApiCdcApplication.java
@@ -0,0 +1,13 @@
+package org.levi.apicdc.apicdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCdcApplication.class, args);
+	}
+
+}
---
+++ api-cdc/src/test/java/org/levi/apicdc/apicdc/ApiCdcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.levi.apicdc.apicdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
