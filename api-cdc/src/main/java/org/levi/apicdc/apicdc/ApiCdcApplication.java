33b39824fb83543c6e9338a806093c553a626de6
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2024-02-28 21:58:50 -0300

    `raw`: adding modelMapper dependency w/ supports for records and @Bean config

--- api-cdc/src/main/java/org/levi/apicdc/apicdc/ApiCdcApplication.java
+++ api-cdc/src/main/java/org/levi/apicdc/apicdc/ApiCdcApplication.java
@@ -1,13 +1,21 @@
 package org.levi.apicdc.apicdc;
 
+
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiCdcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiCdcApplication.class, args);
-	}
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCdcApplication.class, args);
+    }
 
 }
